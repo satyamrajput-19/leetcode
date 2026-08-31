d025c7469d760044b5649a3e231fe30315f9f876
Satyam Rajput <satyamrajput.cse@gmail.com>
2026-08-31 21:25:58 +0530

    Time: 4 ms (62.20%) | Memory: 13.8 MB (82.61%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+
+        vector<string> rows(min(numRows, static_cast<int>(s.length())));
+        int currRow = 0;
+        bool goingDown = false;
+
+        for (char c : s) {
+            rows[currRow] += c;
+            if (currRow == 0 || currRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            currRow += goingDown ? 1 : -1;
+        }
+
+        string result;
+        for (const string& row : rows) {
+            result += row;
+        }
+        return result;
+    }
+};
