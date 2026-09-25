d3f138a42afc57bb549a0e9e48c3f0e97fe326ba
Satyam Rajput <satyamrajput.cse@gmail.com>
2026-09-25 10:51:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (82.42%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
+    int solve(int k,bool res) {
+        if(k==1)
+            return res?1:0;
+        if(k%2==1){
+            return solve(k/2+1,res);
+        }else
+            return solve(k/2,!res);
+    }
     int kthGrammar(int n, int k) {
-        return __builtin_popcount(k - 1) % 2;
+        bool res=false;
+        return solve(k,res);
     }
 };
